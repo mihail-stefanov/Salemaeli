262e5d2678f9af3e955449870d8388d1b4c2225e
Damyan Grancharov <damyan.grancharov@gmail.com>
2015-09-20 15:58:15 +0300

    Stats class

---
+++ Salemaeli_Game/src/game/Stats.java
@@ -0,0 +1,30 @@
+package game;
+
+public class Stats {
+    private int score;
+    private int numberOfLives;
+
+    public Stats (int lives) {
+        setNumberOfLives(lives);
+        setScore(0);
+    }
+
+    public int getNumberOfLives() {
+        return this.numberOfLives;
+    }
+
+    public void setNumberOfLives(int numberOfLives) {
+        if(numberOfLives < 0){
+            throw new IllegalArgumentException("Number of lives must be a positive integer.");
+        }
+        this.numberOfLives = numberOfLives;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setScore(int currentScore) {
+        this.score += currentScore;
+    }
+}
